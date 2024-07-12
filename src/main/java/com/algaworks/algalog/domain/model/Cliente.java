0bec792e55c0a6f06016e51aca62f00a20bccf0b
Marabá79 <iliveiravinicius2503@gmail.com>
2024-07-12 16:43:11 -0400

    deletando linhas desnecessarias

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -11,7 +11,6 @@ import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
